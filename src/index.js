6e199d2f952cd99ba9aa8f221fe2c49bc23fe3f4
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-02-15 16:33:00 +0300

    ready to deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/main.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/main.css";
+import App from "./App";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if ((process.env.NODE_ENV = "production")) {
+  disableReactDevTools();
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
